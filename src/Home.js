2bcc23d7ab28f4519304a3aa4875cb846b12bc2d
Haroon <haroon20@hotmail.co.uk>
2022-04-09 11:20:30 +0100

    Usestate hook lesson 8

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,24 @@
+import { useState } from 'react';
+
 const Home = () => {
+    // name = mario initially setName makes react listen for changes 
+     //in the variable name (due to events happening
+    //on screen user clicks ect) and respond whenn it changes
+    const [name, setName] = useState("mario");
+    const [age, setAge] = useState(25);
 
-    const handleClick = (e) => {
-        console.log('hello ninjas', e);
-    }
-
-    const handleClickAgain = (name,e) => {
-        console.log(`hello ${name}`, e.target);
+    const handleClick = () => {
+        // since we use setName react responds and changes the value of the name variable
+        setName('Luigi')
+        setAge(30);
     }
 
     return ( 
         <div className="Home">
             <h2>Homepage</h2>
+            <p>{name}</p>
+            <p>Is {age} years old</p>
             <button onClick = {handleClick}>Click me</button>
-            <button onClick = {(e) => handleClickAgain('yoshi',e)}>Click me again</button>
         </div>
      );
 }
